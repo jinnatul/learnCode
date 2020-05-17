2906f86792454ec6491317e728847c4910b54043
jinnatul <morolswediu@gmail.com>
2020-05-17 06:16:48 +0600

    numbers in python

---
+++ python/pLevel_1/numbers.py
@@ -0,0 +1,29 @@
+# Sum in two numbers
+print (3 + 2)
+a = 5
+b = 2
+print (a + b)
+
+# Subtraction in two numbers
+print (5 - 3) # 2
+a = 5
+b = 2
+print (a - b) # 3
+
+# Multification in two numbers
+print (5 * 3) # 15
+a = 5
+b = 2
+print (a * b) # 10
+
+# Division in two numbers
+print (5 / 3) # 1
+a = 5.0 # if set one fraction number then got fraction ans or not
+b = 2
+print (a / b) # 2.5
+
+# Mode in two numbers
+print (5 % 3) # 2
+a = 5
+b = 2
+print (a % b) # 1
