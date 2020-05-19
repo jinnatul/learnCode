0e8d3523fc0376cf46f203fbb30a10407d2f66a8
jinnatul <morolswediu@gmail.com>
2020-05-19 20:56:34 +0600

    modules and packages in python

---
+++ python/pLevel_2/myTestModule.py
@@ -0,0 +1,2 @@
+def functionInModule():
+    print ('Hi Morol from inside myTestModule.py')
---
+++ python/pLevel_2/myTestProgram.py
@@ -0,0 +1,15 @@
+# Import 1st Way 
+import myTestModule
+myTestModule.functionInModule()
+
+# Import 2nd Way 
+import myTestModule as morol
+morol.functionInModule()
+
+# Import 3rd Way 
+from myTestModule import functionInModule
+functionInModule()
+
+# Import 4th Way 
+from myTestModule import *
+functionInModule()
