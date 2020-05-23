60b2ddc1f376e2edf9458667705814749df6ccf3
jinnatul <morolswediu@gmail.com>
2020-05-23 06:22:29 +0600

    Boolean Logic in js

---
+++ javaScript/basics/5_BooleanLogic.js
@@ -0,0 +1,18 @@
+/* Boolean Logic */
+
+let name = 'Morol';
+let age = 24;
+
+if (age < 13) {
+    console.log(name + ' is a boy.')
+}
+else if (age >= 13 && age < 20) {
+    console.log(name + ' is a teenager.')
+}
+else if (age >= 20 && age < 30) {
+    console.log(name + ' is a young man.')
+}
+else {
+    console.log(name + ' is a man.')
+}
+// Morol is a young man.
