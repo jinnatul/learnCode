db102938c48b1d8aa5317808afbeaae2f88d5673
jinnatul <morolswediu@gmail.com>
2020-05-28 19:38:01 +0600

    file read write Asynchronous way in node

--- nodeJs/1_vegitableShop/index.js
+++ nodeJs/1_vegitableShop/index.js
@@ -1,7 +1,18 @@
 const fs = require('fs')
 
-const output = fs.readFileSync('./files/text.txt', 'utf-8')
+// Synchronous = Blocking
+const output = fs.readFileSync('./files/input.txt', 'utf-8')
 console.log(output)
-
 const writeData = `${output}. write time: ${Date.now()}`
 fs.writeFileSync('./files/output.txt', writeData)
+
+
+// Asynchronous = Non-Blocking
+fs.readFile('./files/input.txt', 'utf-8', (err, data) => {
+    if (err) return console.log('Some problem here :(')
+    console.log(data)
+    const writeData = `${data}. write time: ${Date.now()}`
+    fs.writeFile('./files/output.txt', writeData, 'utf-8', (err) => {
+        console.log('Write success')
+    })
+})
