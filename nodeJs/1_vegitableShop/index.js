e7071cab8db78c326ada5293be131213f1ce5bed
jinnatul <morolswediu@gmail.com>
2020-05-28 20:01:37 +0600

    routing in nodejs

--- nodeJs/1_vegitableShop/index.js
+++ nodeJs/1_vegitableShop/index.js
@@ -1,6 +1,24 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
 // server create
 const server = http.createServer((req, res) => {
-    res.end('Hello Morol from server')
+    const pathName = req.url;
+    if (pathName == '/' || pathName == '/overview') {
+        res.end('This is overview page')
+    }
+    else if (pathName == '/product') {
+        res.end('This is product page')
+    }
+    else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        })
+        res.end('<h1>Page not found!</h1>')
+    }
+    
 })
 
 server.listen(8000, '127.0.0.1', () => {
