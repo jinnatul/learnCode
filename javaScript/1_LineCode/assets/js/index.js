8bc06d8ab24bec8932f661316e8fe43ef0065ec9
jinnatul <morolswediu@gmail.com>
2020-05-29 15:53:49 +0600

    fixed some design

--- javaScript/1_LineCode/assets/js/index.js
+++ javaScript/1_LineCode/assets/js/index.js
@@ -1,5 +1,28 @@
 $(function() {
+    $("main").hide();
+    setTimeout(function() {
+        $('.mainLoader').fadeOut();
+        $("main").show();
+    }, 1000);
+    
+
+    // collapsible
     $(document).ready(function(){
-        $('.collapsible').collapsible();
+        let elem = document.querySelector('.collapsible.expandable');
+        M.Collapsible.init(elem, {
+          accordion: false
+        });
+    });
+
+    // Smooth scrolling
+    var scrollLink = $(".scroll");
+    scrollLink.click(function(e) {
+        e.preventDefault();
+        $("body,html").animate(
+        {
+            scrollTop: $(this.hash).offset().top
+        },
+        2000
+        );
     });
 })
