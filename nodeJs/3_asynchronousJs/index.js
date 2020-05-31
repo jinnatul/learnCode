afc3d6eac15cd86d8b109b7a04667f78d9bdcd3d
jinnatul <morolswediu@gmail.com>
2020-05-31 23:43:55 +0600

    callback hell to promises

--- nodeJs/3_asynchronousJs/index.js
+++ nodeJs/3_asynchronousJs/index.js
@@ -1,6 +1,7 @@
 let fs = require('fs');
 let superagent = require('superagent');
 
+/*
 fs.readFile(`${__dirname}/test.txt`, 'utf-8', (err, data) => {
   console.log(data);
   superagent
@@ -13,5 +14,23 @@ fs.readFile(`${__dirname}/test.txt`, 'utf-8', (err, data) => {
         console.log('Dog image save to a file.')
       })
     })
-
 })
+*/
+
+// callbacks callback hell to promises
+fs.readFile(`${__dirname}/test.txt`, 'utf-8', (err, data) => {
+  console.log(data);
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then(res => {
+      console.log(res.body);
+
+      fs.writeFile('output.txt', res.body.message, err => {
+        if (err) return console.log(err.message);
+        console.log('Dog image save to a file.');
+      });
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
