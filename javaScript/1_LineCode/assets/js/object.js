12b816c79735aec8ce2469e89c6d282c72e2a2e4
jinnatul <morolswediu@gmail.com>
2020-06-01 11:49:11 +0600

    add object problems

--- javaScript/1_LineCode/assets/js/object.js
+++ javaScript/1_LineCode/assets/js/object.js
@@ -2,54 +2,59 @@ $(function() {
 
     let problems = "<ul class='collapsible expandable'>"
 
-    // 
+    // Check if a value is a plain object
     + "<li><div class='collapsible-header'>"
-    + "<h6>$ </h6></div>"
+    + "<h6>$ Check if a value is a plain object</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Check if a value is an object
     + "<li><div class='collapsible-header'>"
-    + "<h6>$ </h6></div>"
+    + "<h6>$ Check if a value is an object</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Check if an object is empty
     + "<li><div class='collapsible-header'>"
-    + "<h6>$ </h6></div>"
+    + "<h6>$ Check if an object is empty</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Check if multiple objects are equal
     + "<li><div class='collapsible-header'>"
-    + "<h6>$ </h6></div>"
+    + "<h6>$ Check if multiple objects are equal</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Create an empty map that does not have properties
     + "<li><div class='collapsible-header'>"
-    + "<h6>$ </h6></div>"
+    + "<h6>$ Create an empty map that does not have properties</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Create an object from the pairs of key and value
     + "<li><div class='collapsible-header'>"
-    + "<h6>$ </h6></div>"
+    + "<h6>$ Create an object from the pairs of key and value</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Get the value at given path of an object
     + "<li><div class='collapsible-header'>"
-    + "<h6>$ </h6></div>"
+    + "<h6>$ Get the value at given path of an object</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Invert keys and values of an object
     + "<li><div class='collapsible-header'>"
-    + "<h6>$ </h6></div>"
+    + "<h6>$ Invert keys and values of an object</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Omit a subset of properties from an object
     + "<li><div class='collapsible-header'>"
-    + "<h6>$ </h6></div>"
+    + "<h6>$ Omit a subset of properties from an object</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Pick a subset of properties of an object
     + "<li><div class='collapsible-header'>"
-    + "<h6>$ </h6></div>"
+    + "<h6>$ Pick a subset of properties of an object</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Shallow copy an object
+    + "<li><div class='collapsible-header'>"
+    + "<h6>$ Shallow copy an object</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
     + "</ul>"
