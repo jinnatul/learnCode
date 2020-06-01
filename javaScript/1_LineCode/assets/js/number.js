ae73af21eff83bf7c839cd1bdfc8852dbe89ab03
jinnatul <morolswediu@gmail.com>
2020-06-01 11:45:09 +0600

    add number problems

--- javaScript/1_LineCode/assets/js/number.js
+++ javaScript/1_LineCode/assets/js/number.js
@@ -2,54 +2,139 @@ $(function() {
 
     let problems = "<ul class='collapsible expandable'>"
 
-    // 
+    // Add an ordinal suffix to a number
     + "<li><div class='collapsible-header'>"
-    + "<h6>--> </h6></div>"
+    + "<h6>--> Add an ordinal suffix to a number</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Calculate fibonacci numbers
     + "<li><div class='collapsible-header'>"
-    + "<h6>--> </h6></div>"
+    + "<h6>--> Calculate fibonacci numbers</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Calculate the average of arguments
     + "<li><div class='collapsible-header'>"
-    + "<h6>--> </h6></div>"
+    + "<h6>--> Calculate the average of arguments</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Calculate the division of arguments
     + "<li><div class='collapsible-header'>"
-    + "<h6>--> </h6></div>"
+    + "<h6>--> Calculate the division of arguments</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Calculate the factorial of a number
     + "<li><div class='collapsible-header'>"
-    + "<h6>--> </h6></div>"
+    + "<h6>--> Calculate the factorial of a number</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Calculate the mod of collection index
     + "<li><div class='collapsible-header'>"
-    + "<h6>--> </h6></div>"
+    + "<h6>--> Calculate the mod of collection index</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Calculate the remainder of division of arguments
     + "<li><div class='collapsible-header'>"
-    + "<h6>--> </h6></div>"
+    + "<h6>--> Calculate the remainder of division of arguments</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Calculate the sum of arguments
     + "<li><div class='collapsible-header'>"
-    + "<h6>--> </h6></div>"
+    + "<h6>--> Calculate the sum of arguments</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Check if a given integer is a prime number
     + "<li><div class='collapsible-header'>"
-    + "<h6>--> </h6></div>"
+    + "<h6>--> Check if a given integer is a prime number</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
-    // 
+    // Check if a number is a power of 2
     + "<li><div class='collapsible-header'>"
-    + "<h6>--> </h6></div>"
+    + "<h6>--> Check if a number is a power of 2</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Check if a number is even
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Check if a number is even</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Check if a number is negative
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Check if a number is negative</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Check if a number is odd
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Check if a number is odd</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Check if a number is positive
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Check if a number is positive</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Clamp a number between two values
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Clamp a number between two values</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Compute the greatest common divisor between two numbers
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Compute the greatest common divisor between two numbers</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Convert a number to equivalent characters
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Convert a number to equivalent characters</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Convert a string to number
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Convert a string to number</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Convert degrees to radians
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Convert degrees to radians</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Generate a random floating point number in given range
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Generate a random floating point number in given range</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Generate a random integer in given range
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Generate a random integer in given range</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Get the arrays of digits from a number
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Get the arrays of digits from a number</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Multiply arguments
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Multiply arguments</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Prefix an integer with zeros
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Prefix an integer with zeros</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Round a number to a given number of digits
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Round a number to a given number of digits</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Subtract arguments
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Subtract arguments</h6></div>"
+    + "<div class='collapsible-body codejs'></div></li>"
+
+    // Truncate a number to a given number of decimal places without rounding
+    + "<li><div class='collapsible-header'>"
+    + "<h6>--> Truncate a number to a given number of decimal places without rounding</h6></div>"
     + "<div class='collapsible-body codejs'></div></li>"
 
     + "</ul>"
