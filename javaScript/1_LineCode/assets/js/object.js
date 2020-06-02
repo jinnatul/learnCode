b4580d689615035f6e121a9a862df54d5d90189c
jinnatul <morolswediu@gmail.com>
2020-06-02 12:26:40 +0600

    if a value is a plain object

--- javaScript/1_LineCode/assets/js/object.js
+++ javaScript/1_LineCode/assets/js/object.js
@@ -5,7 +5,7 @@ $(function() {
     // Check if a value is a plain object
     + "<li><div class='collapsible-header'>"
     + "<h6>$ Check if a value is a plain object</h6></div>"
-    + "<div class='collapsible-body codejs'></div></li>"
+    + "<div class='collapsible-body codejs'>"+ if_A_ValueIs_A_PlainObject() +"</div></li>"
 
     // Check if a value is an object
     + "<li><div class='collapsible-header'>"
@@ -62,3 +62,19 @@ $(function() {
     $('#pro_object').html(problems);
 
 })
+
+function if_A_ValueIs_A_PlainObject() {
+    let code = "<code class='javascript'><span class='codejs-keyword'>let </span>"
+    + "isPlainObject = v => (!!v && typeof v === '<span class='codejs-keyword'>object</span>' "
+    + "&& (v.<span class='codejs-other'>__proto__</span> === null || v.<span class='codejs-other'>"
+    + "__proto__</span> === <span class='codejs-keyword'>Object</span>.<span class='codejs-defMethod'>"
+    + "prototype</span>));<br><br>// Examples<br>"
+    + "<span class='codejs-function'>isPlainObject</span>(null);                    // false<br>"
+    + "<span class='codejs-function'>isPlainObject</span>('hello world');           // false<br>"
+    + "<span class='codejs-function'>isPlainObject</span>([]);                      // false<br>"
+    + "<span class='codejs-function'>isPlainObject</span>(Object.create(null));     // false<br>"
+    + "<span class='codejs-function'>isPlainObject</span>(function() {});           // false<br>"
+    + "<span class='codejs-function'>isPlainObject</span>({});                      // true<br>"
+    + "<span class='codejs-function'>isPlainObject</span>({ a: '1', b: '2' });      // true<br>"
+    return code;
+}
